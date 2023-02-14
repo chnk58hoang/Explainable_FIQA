d0663ec16065d82dd00381f08bf927d0def199c9
Trần Minh Hoàng (Artorias) <71833423+chnk58hoang@users.noreply.github.com>
2023-02-14 22:54:06 +0700

    change reduction

--- callback/callback.py
+++ callback/callback.py
@@ -8,25 +8,20 @@ class MyCallBack(pl.Callback):
         super().__init__()
         self.test_loader = test_loader
         self.val_loader = val_loader
-        self.mae = nn.L1Loss(reduction='sum')
+        self.mae = nn.L1Loss()
 
 
     def on_validation_end(self, trainer: "pl.Trainer", pl_module: "pl.LightningModule"):
         sharp_mae = 0.0
         illu_mae = 0.0
-        num_samples = 0
         for batch_idx, batch in tqdm(enumerate(self.val_loader)):
             image, sharpness, illu = batch
-            num_samples += image.size(0)
             sharpness = sharpness.to(pl_module._device)
             illu = illu.to(pl_module._device)
             _, pred_sharp, pred_illu = pl_module(image,sharpness,illu)
 
             sharp_mae += self.mae(sharpness, pred_sharp)
             illu_mae += self.mae(illu, pred_illu)
-
-        sharp_mae /= num_samples
-        illu_mae /= num_samples
 
         print('MAE for sharpness {}'.format(float(sharp_mae)))
         print('MAE for illumination {}'.format(float(illu_mae)))
